61c9c1eb11a92936a10872e288c5d8c08692ba84
Rodion Sibov <rodionsibov@gmail.com>
2021-10-26 00:29:39 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const program = require('commander')
+const chalk = require('chalk')
 const createPassword = require('./utils/createPassword')
 
 program.version('1.0.0').description('Simple Password Generator')
@@ -14,4 +15,4 @@ const { length, save, numbers, symbols } = program.opts()
 
 const generatedPassword = createPassword(length, numbers, symbols)
 
-console.log(generatedPassword);
+console.log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
