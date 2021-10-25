b206d6d9f1f9aef8a09ddbb229d840a4f39af5dd
Rodion Sibov <rodionsibov@gmail.com>
2021-10-25 23:44:46 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const program = require('commander')
 program.version('1.0.0').description('Simple Password Generator')
 
 program.
-option('-l, --length <number>', 'length of password', '8')
-.option('-s, --save', 'save password to passwords.txt')
-.parse()
-console.log(program.opts());
+    option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to passwords.txt')
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse()
+
+const { length, save, numbers, symbols } = program.opts()
+console.log(numbers, symbols);
