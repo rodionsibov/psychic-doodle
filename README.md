e6d10ae353feca99121384d49f00f57d7fc7063b
Rodion Sibov <rodionsibov@gmail.com>
2021-10-26 01:03:40 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # psychic-doodle
+## Project setup
+### Install dependencies
+```
+npm install
+```
+
+### Run file
+```
+node index (options)
+```
+
+To create a symlink to run "passgen" from anywhere
+```
+npm link
+
+# Now you can run
+passgen (options)
+
+# To remove symlink
+npm unlink
+```
+
+## Options
+```
+passgen -h
+```
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 const program = require('commander')
 const chalk = require('chalk')
-const clipboardy = require('clipboardy')
+// const clipboardy = require('clipboardy')
 const createPassword = require('./utils/createPassword')
 const savePassword = require('./utils/savePassword')
 
@@ -20,7 +21,7 @@ if (save) {
     savePassword(generatedPassword)
 }
 
-clipboardy.writeSync(generatedPassword)
+// clipboardy.writeSync(generatedPassword)
 
 console.log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
 console.log(chalk.yellow('Password copied to clipboard'));
