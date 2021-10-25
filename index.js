c9dfda3d002f631eb9030e3b1aad21a5de45b64d
Rodion Sibov <rodionsibov@gmail.com>
2021-10-26 00:43:59 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const program = require('commander')
 const chalk = require('chalk')
+const clipboardy = require('clipboardy')
 const createPassword = require('./utils/createPassword')
+const savePassword = require('./utils/savePassword')
 
 program.version('1.0.0').description('Simple Password Generator')
 
@@ -14,5 +16,11 @@ program.
 const { length, save, numbers, symbols } = program.opts()
 
 const generatedPassword = createPassword(length, numbers, symbols)
+if (save) {
+    savePassword(generatedPassword)
+}
+
+clipboardy.writeSync(generatedPassword)
 
 console.log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+console.log(chalk.yellow('Password copied to clipboard'));
