3b744fd3e0b9556555664d4ba41cdcb259ab3528
Rodion Sibov <rodionsibov@gmail.com>
2021-10-26 00:25:52 +0300

    update

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -9,7 +9,7 @@ const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
     return generatePassword(length, chars)
 }
 
-const generatedPassword = (length, chars) => {
+const generatePassword = (length, chars) => {
     let password = ''
     for (let i = 0; i < length; i++) {
         password += chars.charAt(Math.floor(Math.random() * chars.length))
