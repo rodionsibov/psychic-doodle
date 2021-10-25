a3fa26a053f14e887897289847436ed0412f6674
Rodion Sibov <rodionsibov@gmail.com>
2021-10-25 23:39:39 +0300

    update

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const program = require('commander')
 
 program.version('1.0.0').description('Simple Password Generator')
 
-program.option('-l, --length <number>', 'length of password').parse()
+program.option('-l, --length <number>', 'length of password', '8').parse()
+console.log(program.opts());
